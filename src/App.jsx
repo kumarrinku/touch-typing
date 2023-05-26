933f286fec53d8195c011e203acd08f0f171224d
kumarrinku <rkm9304677599@gmail.com>
2023-05-26 23:55:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,176 @@
+import { useState, useEffect, useRef } from 'react'
+const NUMB_OF_WORDS = 500
+const SECONDS = 60 * 5
+
+function App() {
+  const [words, setWords] = useState([])
+  const [countDown, setCountDown] = useState(SECONDS)
+  const [currInput, setCurrInput] = useState("")
+  const [currWordIndex, setCurrWordIndex] = useState(0)
+  const [currCharIndex, setCurrCharIndex] = useState(-1)
+  const [currChar, setCurrChar] = useState("")
+  const [correct, setCorrect] = useState(0)
+  const [incorrect, setIncorrect] = useState(0)
+  const [status, setStatus] = useState("waiting")
+  const textInput = useRef(null)
+
+  useEffect(() => {
+    setWords(generateWords())
+  }, [])
+
+  useEffect(() => {
+    if (status === 'started') {
+      textInput.current.focus()
+    }
+  }, [status])
+  const generateNextKeys = () => {
+    const keys = 'asdfjkl;';
+    const nextKeys = Array.from({ length: 1 }, () =>
+      keys[Math.floor(Math.random() * keys.length)]
+    ).join('');
+    return nextKeys;
+  };
+
+  function generateWords() {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => generateNextKeys())
+  }
+
+  function start() {
+
+    if (status === 'finished') {
+      setWords(generateWords())
+      setCurrWordIndex(0)
+      setCorrect(0)
+      setIncorrect(0)
+      setCurrCharIndex(-1)
+      setCurrChar("")
+    }
+
+    if (status !== 'started') {
+      setStatus('started')
+      let interval = setInterval(() => {
+        setCountDown((prevCountdown) => {
+          if (prevCountdown === 0) {
+            clearInterval(interval)
+            setStatus('finished')
+            setCurrInput("")
+            return SECONDS
+          } else {
+            return prevCountdown - 1
+          }
+        })
+      }, 1000)
+    }
+
+  }
+
+  function handleKeyDown({ keyCode, key }) {
+    // space bar 
+    if (keyCode === 32) {
+      checkMatch()
+      setCurrInput("")
+      setCurrWordIndex(currWordIndex + 1)
+      setCurrCharIndex(-1)
+      // backspace
+    } else if (keyCode === 8) {
+      setCurrCharIndex(currCharIndex - 1)
+      setCurrChar("")
+    } else {
+      setCurrCharIndex(currCharIndex + 1)
+      setCurrChar(key)
+    }
+  }
+
+  function checkMatch() {
+    const wordToCompare = words[currWordIndex]
+    const doesItMatch = wordToCompare === currInput.trim()
+
+    if (doesItMatch) {
+      setCorrect(correct + 1)
+    } else {
+      setIncorrect(incorrect + 1)
+    }
+  }
+
+  function getCharClass(wordIdx, charIdx, char) {
+    if (wordIdx === currWordIndex && charIdx === currCharIndex && currChar && status !== 'finished') {
+      if (char === currChar) {
+        return 'has-background-success'
+      } else {
+        return 'has-background-danger'
+      }
+    } else if (wordIdx === currWordIndex && currCharIndex >= words[currWordIndex].length) {
+      return 'has-background-danger'
+    } else {
+      return ''
+    }
+  }
+
+
+  return (
+    <div className="App" >
+      <h1 style={{ fontSize: "2rem", fontWeight: "bold", marginLeft: "5%" }}>Touch Typing</h1>
+      <div>
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>{countDown}</h2>
+        </div>
+      </div>
+
+      {status === 'started' && (
+        <section style={{ width: "70%", marginLeft: "15%" }}>
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, i) => (
+                  <span key={i}>
+                    <span>
+                      {word.split("").map((char, idx) => (
+                        <span className={getCharClass(i, idx, char)} key={idx}>{char}</span>
+                      ))}
+                    </span>
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+      {status === 'finished' && (
+        <div className="section">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <p className="is-size-5">Words per minute:</p>
+              <p className="has-text-primary is-size-1">
+                {correct}
+              </p>
+            </div>
+            <div className="column has-text-centered">
+              <p className="is-size-5">Accuracy:</p>
+              {correct !== 0 ? (
+                <p className="has-text-info is-size-1">
+                  {Math.round((correct / (correct + incorrect)) * 100)}%
+                </p>
+              ) : (
+                <p className="has-text-info is-size-1">0%</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="control" style={{ width: "30%", marginLeft: "35%", marginTop: "2rem", border: "1px solid black", borderRadius: "5px" }}>
+        <input ref={textInput} disabled={status !== "started"} type="text" className="input" onKeyDown={handleKeyDown} value={currInput} onChange={(e) => setCurrInput(e.target.value)} />
+      </div>
+      {status !== 'started' && (
+        <div style={{ width: "10%", marginLeft: "45%", marginTop: "2rem" }}>
+          <button className="button is-info is-fullwidth" onClick={start}>
+            Start
+          </button>
+        </div>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
